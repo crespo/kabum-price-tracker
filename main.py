ff531382c4a53a674ffcfcaef54638a89d9bcc0a
raul crespo <raulcrespo@outlook.com.br>
2022-02-08 20:54:20 -0300

    added try&except if fails to find element

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 def main():
-    url = 'https://www.kabum.com.br/produto/15296/cartucho-de-tinta-epson-133-ciano-t133220'
-
+    url = 'http://google.com/'
     browser = get_web_driver()
     browser.get(url)
 
-    name = browser.find_element(By.XPATH, '//*[@id="__next"]/main/article/section/div[2]/h1').text
-    price = browser.find_element(By.XPATH, '//*[@id="blocoValores"]/div[3]/b').text\
-        .replace("R$ ", "").replace(".", "").replace(",", ".")
+    try:
+        name = browser.find_element(By.XPATH, '//*[@id="__next"]/main/article/section/div[2]/h1').text
+    except selenium.common.exceptions.NoSuchElementException:
+        name = 'Not Found'
+
+    try:
+        price = browser.find_element(By.XPATH, '//*[@id="blocoValores"]/div[3]/b').text\
+            .replace("R$ ", "").replace(".", "").replace(",", ".")
+    except selenium.common.exceptions.NoSuchElementException:
+        price = 'Not Found'
 
     print("name: " + name +
-          "\nprice: R$" + price)
+          "\nprice: " + price)
 
 
 def get_web_driver():
